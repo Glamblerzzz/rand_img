2ae0f1dc446f18607fc53e683122f41a18859dea
Glambler <156225687@qq.com>
2020-07-22 16:39:44 +0800

    first

---
+++ miniprogram/pages/rand_tb_img/rand_tb_img.js
@@ -0,0 +1,75 @@
+// pages/rand_tb_img/rand_tb_img.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    wx.request({
+      url: 'https://api.66mz8.com/api/rand.tbimg.php?format=json',
+      success: res => {
+        console.log(res.data.pic_url);
+        this.setData({
+          url: res.data.pic_url
+        })
+      }
+    }
+    )
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
